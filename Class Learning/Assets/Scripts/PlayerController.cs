60c3989e946c07accbb8e7dcd03cd5d1b080b0a5
BeelzebubBoy <jardinrantanen@gmail.com>
2022-02-21 16:00:51 +0800

    Core functionality for an interaction system added core functionality for a health bar system added crouch cleanup up isGrounded code

--- Class Learning/Assets/Scripts/PlayerController.cs
+++ Class Learning/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public float gravity = 3.5f;
     public float speed = 2f; //normal player speed
+    [Range(0.1f, 1f)]
+    public float crouchSpeedMultipler = 0.5f;
     public float jumpForce = 0.5f;
 
     private CharacterController controller;
     private Vector3 motion;
     private float currentSpeed = 0;
     private float velocity = 0;
+    private bool crouching = false;
+    private bool isGrounded = false;
 
     //Awake is called before start method
     private void Awake()
@@ -26,10 +30,10 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        isGrounded = controller.isGrounded;
         motion = Vector3.zero;
-        bool grounded = controller.isGrounded;
 
-        if(grounded == true)
+        if(isGrounded == true)
         {
             velocity = -gravity * Time.deltaTime;
         }
@@ -42,16 +46,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(controller.isGrounded == true)
+        if(isGrounded == true)
         {
-            if(Input.GetKeyDown(KeyCode.Space) == true)
+            if (crouching == false)
             {
-                velocity = jumpForce;
+                if (Input.GetKeyDown(KeyCode.Space) == true)
+                {
+                    velocity = jumpForce;
+                }
+                else if (Input.GetKeyDown(KeyCode.C) == true)
+                {
+                    crouching = true;
+                    currentSpeed = speed * crouchSpeedMultipler;
+                    controller.height = 1;
+                }
+            }
+           
+        }
+        if (crouching == true)
+        {
+            if (Input.GetKeyUp(KeyCode.C) == true)
+            {
+                crouching = false;
+                currentSpeed = speed;
+                controller.height = 2;
             }
         }
         ApplyMovement();
     }
-
     void ApplyMovement()
     {
         float inputX = Input.GetAxisRaw("Vertical") * currentSpeed;
