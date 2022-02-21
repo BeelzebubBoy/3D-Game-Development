8d929bea4a5f8535e02a8f720b758a74e3cfd59a
BeelzebubBoy <jardinrantanen@gmail.com>
2022-02-21 16:20:46 +0800

    added health potion

--- Class Learning/Assets/Scripts/Health.cs
+++ Class Learning/Assets/Scripts/Health.cs
@@ -9,6 +9,22 @@ public class Health : MonoBehaviour
     public Image healthBar;
 
     private float currentHealth = 0;
+
+    public static Health Instance { get; private set; }
+
+    private void Awake()
+    {
+        //set our singleton value
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            enabled = false;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
